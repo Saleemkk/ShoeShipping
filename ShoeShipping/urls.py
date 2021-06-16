0133f0f3e3756d508c73b512d53950f68a3e03dc
Angala Sube <angalasube@TingsoundsdeMacBook-Pro.local>
2021-06-16 11:39:47 +0500

    push

--- ShoeShipping/urls.py
+++ ShoeShipping/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import  static
 from django.conf import  settings
 
 urlpatterns = [
-    path('', include('shipping.urls')),
+    path(r'^$', include('shipping.urls')),
     path('admin/', admin.site.urls),
 ]
 
